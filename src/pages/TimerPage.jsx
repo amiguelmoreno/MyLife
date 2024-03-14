69a9be68d2087ba7c52f787b9556d55f990d31d2
Miguel Moreno <amiguelmoreno42@gmail.com>
2024-03-14 04:08:22 +0100

    fix screen size in mobile scroll

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -7,14 +7,12 @@ import { FormTimerProvider } from "../contexts/useFormTimerContext";
 function TimerPage() {
   return (
     <div
-      className={`relative flex items-center justify-center flex-col gap-8  p-4  bg-[rgb(255,_255,_255)] bg-[url("/icons/timer-background.png")] bg-cover bg-bottom`}
+      className={`relative flex items-center justify-center flex-col gap-8 h-[100vh] overflow-hidden p-4  bg-[rgb(255,_255,_255)] bg-[url("/icons/timer-background.png")] bg-cover bg-bottom`}
     >
       <FormTimerProvider>
-        <div className='absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2'>
-          <TimerDisplay></TimerDisplay>
-          <TimerControls></TimerControls>
-          <TimerQuotes></TimerQuotes>
-        </div>
+        <TimerDisplay></TimerDisplay>
+        <TimerControls></TimerControls>
+        <TimerQuotes></TimerQuotes>
         <SpotyBtn></SpotyBtn>
       </FormTimerProvider>
     </div>
